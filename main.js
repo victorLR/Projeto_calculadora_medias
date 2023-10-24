9b97851396fee57178480b5f393b6de866390586
victor Affonso <victoraffonso1997@gmail.com>
2023-10-24 11:03:16 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,66 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado"/>';
+const atividades = [];
+const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima"));
+
+let linhas =''; /*para que o conteúdo seja mantido este precisa estar no topo, no ponto global do arquivo java*/
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+});
+
+function adicionaLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert (`A atividade: ${inputNomeAtividade.value} já foi inserida`); /*'includes'determina se um conjunto de caracteres pode ser encontrado dentro de outra string, retornando true ou false*/
+    } 
+    
+    else {
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value)); /*parseFloat serve para dizer q o numero não é uma string fazendo ser possivel uma soma das notas.*/ 
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha +=`<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`; /*adiciona um seletor em que se o numero for igual ou maior q 7 sera aprovado, (menos, sera reprovado.) */
+    linha +=`</tr>`; 
+
+    linhas += linha;
+    }
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas [i];
+    }
+
+
+    return somaDasNotas / notas.length;
+}
